c06d07db02f8b89efd2fcb97617abc5539f13f56
chummer80 <mike.c.chu@gmail.com>
2014-11-10 14:33:38 -0800

    sketch thing works
    button needs to be centered

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
 
+	function buildGrid(size) {
+		var $gridRow, $gridCell, i, j;
+		var sizePercentage = (100 / size) + "%";
 
+		for (i = 0; i < size; i++) {
+			$gridRow = $("<div></div>");
+			$gridRow.addClass("grid_row");
+			$gridRow.css("height", sizePercentage);
+
+			for (j = 0; j < size; j++) {
+				$gridCell = $("<div></div>");
+				$gridCell.addClass("grid_cell");
+				$gridCell.css("width", sizePercentage);
+				$gridCell.mouseenter(function() {
+					$(this).addClass("filled");
+				});
+				$gridRow.append($gridCell);
+			}
+
+			$("#grid_container").append($gridRow);
+		}
+	}
+
+	$("#new_grid_button").click(function() {
+		var gridSize = prompt("How many squares per side?");
+
+		if ((gridSize % 1 === 0) && (gridSize > 0)){
+			$("#grid_container").empty();
+			buildGrid(gridSize);
+		}
+		else {
+			alert("Input error: please enter a positive number.");
+		}
+	});
+
+
+	buildGrid(16);
 });
