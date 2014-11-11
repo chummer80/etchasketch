cfa98c65803fbfd23f1f5f7d6d0c121820dfcc9b
chummer80 <mike.c.chu@gmail.com>
2014-11-10 16:25:46 -0800

    colorful sketchpad
    - cells are filled with random color.
    - color darkens on each cursor pass through
    - use flex container to align elements properly

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+	function randomColor() {
+		var r = Math.floor(Math.random() * 255);
+		var g = Math.floor(Math.random() * 255);
+		var b = Math.floor(Math.random() * 255);
+		return "rgb(" + r + "," + g + "," + b + ")";
+	}
+
+	function fillCell() {
+		var cell = $(this);
+		var rgb, rgbArray, r, g, b;
+
+		if (cell.hasClass("filled")) {
+			rgb = cell.css("background-color");
+			rgbArray = rgb.match(/[0-9]+/g);;
+			r = parseInt(rgbArray[0]);
+			g = parseInt(rgbArray[1]);
+			b = parseInt(rgbArray[2]);
+
+			//darken colors by 10%;
+			r = Math.floor(r - 25, 0);
+			g = Math.floor(g - 25, 0);
+			b = Math.floor(b - 25, 0);
+			rgb = "rgb(" + r + "," + g + "," + b + ")";
+			cell.css("background-color", rgb);
+		}
+		else {
+			cell.addClass("filled");
+			cell.css("background-color", randomColor());
+		}
+
+	}
+
 	function buildGrid(size) {
 		var $gridRow, $gridCell, i, j;
 		var sizePercentage = (100 / size) + "%";
@@ -13,9 +45,7 @@ $(document).ready(function() {
 				$gridCell = $("<div></div>");
 				$gridCell.addClass("grid_cell");
 				$gridCell.css("width", sizePercentage);
-				$gridCell.mouseenter(function() {
-					$(this).addClass("filled");
-				});
+				$gridCell.mouseenter(fillCell);
 				$gridRow.append($gridCell);
 			}
 
